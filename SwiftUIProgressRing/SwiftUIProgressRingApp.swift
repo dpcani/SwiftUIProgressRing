1cdcc97125d625c30de7e70f15830800a1c61efa
David de la Puente <david.puente@marbleinteractive.com>
2023-02-15 09:14:07 +0100

    start project

---
+++ SwiftUIProgressRing/SwiftUIProgressRingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProgressRingApp.swift
+//  SwiftUIProgressRing
+//
+//  Created by Marble Interactive SL on 15/2/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProgressRingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
